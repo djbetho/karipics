20121671a60e250eae0481858caf366dacfbde36
Ruperto <ru.bugueno@gmail.com>
2023-07-20 01:19:32 -0400

    mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,9 +36,30 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+ # Don't care if the mailer can't send.
+ config.action_mailer.raise_delivery_errors = false
+
+ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+ 
+ config.action_mailer.perform_caching = false
+
+ config.action_mailer.perform_caching = false
+ config.action_mailer.delivery_method = :sendmail
+ config.action_mailer.perform_deliveries = true
+ config.action_mailer.raise_delivery_errors = true
+ config.action_mailer.default_options = {from: 'no-reply@testemail.com'}
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: 'ru.bugueno@gmail.com',
+  password: 'wtootqubqgjviasi',
+  authentication: 'plain',
+  enable_starttls_auto: true
+ }
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
