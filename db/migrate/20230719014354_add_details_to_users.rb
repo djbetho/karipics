131c14e5ddda107d633b8f06bd3fe3796334f1e3
Ruperto <ru.bugueno@gmail.com>
2023-08-10 01:25:08 -0400

    seed

--- db/migrate/20230719014354_add_details_to_users.rb
+++ db/migrate/20230719014354_add_details_to_users.rb
@@ -1,6 +1,6 @@
-class AddDetailsToUsers < ActiveRecord::Migration[7.0]
+class AddDetailsToUseers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :name, :string
-    add_column :users, :phone, :string
+    add_column :useers, :name, :string
+    add_column :useers, :phone, :string
   end
 end
