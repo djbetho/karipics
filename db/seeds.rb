e77cb53356eea1b4caf1af8c6ed7f2e70ed24822
Ruperto <ru.bugueno@gmail.com>
2023-08-10 01:29:53 -0400

    prueba error seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-<<<<<<< HEAD
-User.create({ name: "Administrador", 
-                email: "ru.bugueno@gmail.com",
-                password: "123456", password_confirmation: "123456",
-                role:2 })
-=======
 
-admin=User.create({ name: "Administrador", email: 'ru.bugueno@gmail.com',
-               password: '123456', password_confirmation: '123456',
-               role:2})
->>>>>>> 34ff1e7c869ffbaecdffabc11bc52e21ccf7cf94
